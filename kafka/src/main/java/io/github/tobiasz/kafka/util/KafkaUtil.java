8ffc8dbaff4a1f00acf16899f516eda711e7da76
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-10-01 16:52:27 +0200

    refactor: move some stuff around

--- kafka/src/main/java/io/github/tobiasz/kafka/util/KafkaUtil.java
+++ kafka/src/main/java/io/github/tobiasz/kafka/util/KafkaUtil.java
@@ -38,7 +38,7 @@ public class KafkaUtil {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapService);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer .class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return new DefaultKafkaProducerFactory<>(props);
 	}
 
